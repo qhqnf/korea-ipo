11c1812dfe9e25f5ef3ae7fb2f74616c883d49e0
yhc <didguscjf95@naver.com>
2021-04-26 21:29:09 +0900

    create skeleton krx module

--- financial_data_api/krx.py
+++ financial_data_api/krx.py
@@ -1,2 +1,32 @@
+import requests
+import datetime
+import re
+import io
+import pandas as pd
+
 class Krx:
-    pass
+    DOWNLOAD_URL = "http://file.krx.co.kr/download.jspx"
+
+    @staticmethod
+    def generate_otp():
+        # params
+        # requests
+        # otp = res.text
+        # return otp
+        pass
+
+    @staticmethod
+    def get_stock_data(otp: str):
+        headers = {
+            "User-Agent": "Mozilla/5.0",
+            "Referer": "http://marketdata.krx.co.kr/mdi",
+        }
+        params = {
+            "code": otp
+        }
+        res = requests.post(Krx.DOWNLOAD_URL, headers=headers, params=params)
+        res.encoding = "utf-8-sig"
+        
+        stock_data = pd.read_csv(io.BytesIO(data.content), header=0, thousands=",").iloc[:, 0:3]
+
+        return stock_data
