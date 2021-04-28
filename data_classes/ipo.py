076ceb50238d056d8760f9e365edbc35035f66b7
yhc <didguscjf95@naver.com>
2021-04-28 16:44:31 +0900

    delete unused line

--- data_classes/ipo.py
+++ data_classes/ipo.py
@@ -23,14 +23,9 @@ class Ipo:
     def __init__(self, **kwargs):
         self.company_code = kwargs.get("company_code")
         self.company_name = kwargs.get("company_name")
-        self.start_date = Formatter.change_date_string_format(kwargs.get("청약공고일"), "%Y년 %m %d")
+        self.start_date = Formatter.change_date_string_format(kwargs.get("청약공고일"))
         self.payment_date = Formatter.change_date_string_format(kwargs.get("납입기일"))
         self.security_company_set = [SecurityCompany(**security_company) for security_company in kwargs.get("security_company_data")]
     
     def asdict(self):
         return asdict(self)
-
-if __name__ == "__main__":
-    data = {'company_code': '00527011', 'company_name': '엘비루셈', '청약기일': '20210603', '납입기일': '20210607', '청약공고일': '20210602', 'security_company_data': [{'인수인': '한국투자증권', '인수수량': 4200000}, {'인수인': 'KB증권', '인수수량': 1800000}]}
-    data = Ipo(**data)
-    print(data.asdict())
